1781f494783b0c748910c5723b0503825416f82a
root <root@alex-cool-app-acs-bootstrap-22c85>
2024-09-12 11:41:50 +0000

    Initial commit

---
+++ src/main/java/root_package/AlexCoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexCoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexCoolAppApplication.class, args);
+	}
+
+}
